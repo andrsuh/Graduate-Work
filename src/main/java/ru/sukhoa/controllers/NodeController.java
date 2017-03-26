7b91336085107820c058171a7014ecd0e206d3be
andrsuh <andrsuh13@yandex.ru>
2017-03-26 16:15:14 +0300

    subtree fetching taked into another controller

--- src/main/java/ru/sukhoa/controllers/NodeController.java
+++ src/main/java/ru/sukhoa/controllers/NodeController.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.sukhoa.domain.Node;
 import ru.sukhoa.service.NodeCreateService;
-import ru.sukhoa.service.NodeFetchService;
 
 import java.util.List;
 
@@ -16,16 +15,9 @@ public class NodeController {
 
     private NodeCreateService nodeCreateService;
 
-    private NodeFetchService nodeFetchService;
-
     @Autowired
     public void setNodeCreateService(NodeCreateService nodeCreateService) {
         this.nodeCreateService = nodeCreateService;
-    }
-
-    @Autowired
-    public void setNodeFetchService(NodeFetchService nodeFetchService) {
-        this.nodeFetchService = nodeFetchService;
     }
 
     @ResponseStatus(HttpStatus.OK)
@@ -33,15 +25,4 @@ public class NodeController {
     public void createNodes(@RequestBody(required = true) final List<Node> nodes) {
         nodeCreateService.createNodes(nodes);
     }
-
-    @RequestMapping(value = "/postgres/subtree", method = RequestMethod.GET)
-    public List<Node> getPostgresSubtreeInRootOf(@RequestParam(name = "id", required = true) final String id) {
-        return nodeFetchService.getPostgresSubtreeInRootOf(id);
-    }
-
-    @RequestMapping(value = "/neo/subtree", method = RequestMethod.GET)
-    public List<Node> getNeoSubtreeInRootOf(@RequestParam(name = "id", required = true) final String id) {
-        return nodeFetchService.getNeoSubtreeInRootOf(id);
-    }
-
 }
