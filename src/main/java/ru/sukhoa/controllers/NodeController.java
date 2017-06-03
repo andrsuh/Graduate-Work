c8d820062032784e0cddc2fd9cc6c6ac50f12d12
andrsuh <andrsuh13@yandex.ru>
2017-06-03 16:47:31 +0300

    minor fixes

--- src/main/java/ru/sukhoa/controllers/NodeController.java
+++ src/main/java/ru/sukhoa/controllers/NodeController.java
@@ -1,8 +1,10 @@
 package ru.sukhoa.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import ru.sukhoa.domain.Node;
 import ru.sukhoa.service.NodeService;
 
@@ -18,38 +20,31 @@ public class NodeController {
         this.nodeService = nodeService;
     }
 
-    @ResponseStatus(HttpStatus.OK)
     @RequestMapping(method = RequestMethod.POST)
-    public void createNodes(@RequestBody final List<Node> nodes) {
+    public void createNodes(@RequestBody List<Node> nodes) {
         nodeService.createNodes(nodes);
     }
 
-    @ResponseStatus(HttpStatus.OK)
     @RequestMapping(value = "postgres", method = RequestMethod.POST)
-    public void createNodesPostgres(@RequestBody final List<Node> nodes) {
+    public void createNodesPostgres(@RequestBody List<Node> nodes) {
         nodes.forEach(nodeService::createNodePostgres);
     }
 
-    @ResponseStatus(HttpStatus.OK)
     @RequestMapping(value = "neo", method = RequestMethod.POST)
-    public void createNodesNeo(@RequestBody final List<Node> nodes) {
+    public void createNodesNeo(@RequestBody List<Node> nodes) {
         nodes.forEach(nodeService::createNodeNeo4j);
     }
 
-    @ResponseStatus(HttpStatus.OK)
     @RequestMapping(method = RequestMethod.PUT)
     public void updateNode(@RequestBody Node updatingNode) {
         nodeService.updateNode(updatingNode);
     }
 
-
-    @ResponseStatus(HttpStatus.OK)
     @RequestMapping(value = "postgres", method = RequestMethod.PUT)
     public void updateNodePostgres(@RequestBody Node updatingNode) {
         nodeService.updateNodePostgres(updatingNode);
     }
 
-    @ResponseStatus(HttpStatus.OK)
     @RequestMapping(value = "neo", method = RequestMethod.PUT)
     public void updateNodeNeo(@RequestBody Node updatingNode) {
         nodeService.updateNodeNeo4j(updatingNode);
